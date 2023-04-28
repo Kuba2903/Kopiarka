8903ad8c9db8096b3a01cb6225a5c9a66add874e
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2023-04-28 19:18:25 +0200

    Wykonano zadanie pierwsze. Wszystkie testy działają

---
+++ Kopiarka-main/Zadanie 1/Copier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        
+        public void Print(in IDocument document)
+        {
+            Copier copier = this;
+            if (copier.state == IDevice.State.on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now} Print: {document}.pdf");
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            document = null;
+
+            Copier copier = this;
+
+            if (copier.state == IDevice.State.on)
+            {
+                if (formatType == IDocument.FormatType.JPG)
+                {
+                    Console.WriteLine($"{DateTime.Now} Scan: ImageScan{ScanCounter}.jpg");
+                }
+
+                if (formatType == IDocument.FormatType.TXT)
+                {
+                    Console.WriteLine($"{DateTime.Now} Scan: TextScan{ScanCounter}.txt");
+                }
+
+                if (formatType == IDocument.FormatType.PDF)
+                {
+                    Console.WriteLine($"{DateTime.Now} Scan: PDFScan{ScanCounter}.pdf");
+                }
+
+                ScanCounter++;
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            Copier copier = this;
+
+            if (copier.state == IDevice.State.on)
+            {
+
+                IDocument document = null;
+                Scan(out document, IDocument.FormatType.JPG);
+
+                Console.WriteLine($"{DateTime.Now} Print: {document}.jpg");
+                PrintCounter++;
+            }
+        }
+    }
+}
---
+++ Kopiarka-main/Zadanie 1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Zadanie_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2, IDocument.FormatType.PDF);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);*/
+
+
+            var copier = new Copier();
+            copier.PowerOn();
+            Console.WriteLine(copier.GetState());
+
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            copier.Print(in doc1);
+        }
+    }
+}
